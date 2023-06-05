f8b6f5868b7ef158ff99a1ea430a6bfc86a34188
Ali Zadil <tarikz94@gmail.com>
2023-06-05 11:06:22 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
 import React from "react";
 import "./App.css";
 import CardComponent from "./CardComponent.jsx";
+import { useState } from "react";
+import { useEffect } from "react";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [items, setItem] = useState([]);
+
+  useEffect(() => {console.log(inputText)},[inputText])
+  useEffect(() => {console.log(items)},[items])
+
   return (
     <div>
-      <CardComponent
-        content={
-          "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam."
-        }
-      ></CardComponent>
+      <input 
+        type="text"
+        onChange={(it) => setInputText(it.target.value)} 
+      />
+      <button 
+        onClick = {() => {
+          let updatedArray = [...items, inputText];
+          setItem(updatedArray);
+        }}
+      > Add New Item
+      </button>
+      <br />
+      <br />
+      {items.map((item) => {
+        return (
+          <CardComponent
+            content={item}
+          ></CardComponent>
+        )
+      })}
     </div>
   );
 }
