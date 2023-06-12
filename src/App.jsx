384b6d544139b28618abc3e3599d20f2f20ae004
Ali Zadil <tarikz94@gmail.com>
2023-06-12 11:51:36 +0300

    trying

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
+import NumberDisplay from './NumberDisplay.jsx'
 import "./App.css";
 
 function App() {
   return (
     <div>
+      {NumberDisplay()}
     </div>
   );
 }
---
+++ src/NumberDisplay.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { resourceLimits } from "worker_threads";
+
+const API_URL = 'https://www.random.org/integers/?num=1&min=1&max=100&col=1&base=10&format=plain&rnd=new'
+
+function NumberDisplay(){
+    const [number, setNumber] = useState(0);
+
+    useEffect (() => {
+        fetch(API_URL).then(response => response.json()).then((result) => {
+            console.log(result)
+        })
+    }, [])
+
+    const saveToStorage = () => {
+        localStorage.setItem('Number', JSON.stringify(number));
+    }
+
+    const loadFromStorage = () => {
+        setNumber(JSON.parse(localStorage.getItem('Number')));
+    }
+
+    return (
+        <div>
+            <p>{number}</p>
+            <br />
+            <button onClick={() => {
+                setNumber(number +1)
+            }}>Increment</button>
+            <button onClick={() => saveToStorage}>Save to storage</button>
+            <button onClick={() => loadFromStorage}>Load from storage</button>
+        </div>
+    )
+
+}
+
+
+export default NumberDisplay
