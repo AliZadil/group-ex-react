244ebab1a7e059988f2ad1fbfaee00b403bb81b6
Ali Zadil <tarikz94@gmail.com>
2023-06-20 14:14:38 +0300

    whyyy

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-
+import { Route, Routes, Link } from 'react-router-dom';
 import NumberDisplay from './NumberDisplay';
+import CardComponent from './CardComponent.jsx';
+import NotFound from './NotFound.jsx';
+import Welcome from './WelcomeComponent.jsx';
 
 function App() {
   return (
     <div>
-      <NumberDisplay></NumberDisplay>
+      <Routes>
+        <Route path='/number' element={<NumberDisplay />} />
+        <Route path='/' element={<Welcome />} />
+        <Route path='*' elemenet={<NotFound />} />
+        <Route path="/card" element={<CardComponent content={"Card"} index={0} />} />
+      </Routes>
+      <br />
+      <link to={'/'}>Home</link>
+      <br />
+      <link to={'/number'}>Number</link>
+      <br />
+      <link to={'/card'}>Card</link>
     </div>
   );
 }
---
+++ src/WelcomeComponent.jsx
@@ -0,0 +1,12 @@
+import { Route, Routes, Link } from 'react-router-dom';
+
+const Welcome = () => {
+    return (
+      <div>
+        <p>Hello</p>
+      </div>
+    );
+  };
+  
+  export default Welcome;
+  
